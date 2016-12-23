815cd33f1fa84c835e475e7178bba8605177f443
Guillaume Hivert <guillaume.hivert@outlook.com>
2016-12-24 00:07:04 +0100

    Class functions & properties

--- lib/proton/web_contents.rb
+++ lib/proton/web_contents.rb
@@ -1,35 +1,70 @@
 require 'native'
 
-class WebContents
-  class << self
-    def get_all_web_contents
-      arr, wcs = [], `webContents.getAllWebContents()`
-      (0...`wcs.length`).each do |i|
-        arr << WebContents.new(wcs.JS[i])
+module Proton
+  class BrowserWindow
+    class WebContents
+      # Class functions
+      class << self
+        def get_all_web_contents
+          arr, wcs = [], `webContents.getAllWebContents()`
+          (0...`wcs.length`).each do |i|
+            arr << WebContents.new(wcs.JS[i])
+          end
+          arr
+        end
+
+        def get_focused_web_contents
+          wc = `webContents.getFocusedWebContents()`
+          if wc
+            WebContents.new(wc)
+          else
+            nil
+          end
+        end
+
+        def from_id(id)
+          wc = `webContents.fromId(id)`
+          if wc
+            WebContents.new(wc)
+          else
+            nil
+          end
+        end
       end
-      arr
-    end
 
-    def get_focused_web_contents
-      wc = `webContents.getFocusedWebContents()`
-      wc.nil? ? nil : WebContents.new(wc)
-    end
+      # Instance functions.
+      def initialize(content)
+        @content = content
+      end
 
-    def from_id(id)
-      wc = `webContents.fromId(id)`
-      wc.nil? ? nil : WebContents.new(wc)
-    end
-  end
+      def send(chan, *rest)
+        if rest.size == 0
+          `#{@content}.send(#{chan})`
+        else
+          rest.each { |var| `#{@content}.send(#{chan}, #{var})` }
+        end
+      end
 
-  def initialize(content)
-    @content = content
-  end
+      # Refactor for better code.
+      def id
+        `#{@content}.id`
+      end
 
-  def send(chan, *rest)
-    if rest.size == 0
-      `#{@content}.send(#{chan})`
-    else
-      rest.each { |var| `#{@content}.send(#{chan}, #{var})` }
+      def session
+        `#{@content}.session`
+      end
+
+      def host_web_contents
+        `#{@content}.hostWebContents`
+      end
+
+      def dev_tools_web_contents
+        `#{@content}.devToolsWebContents`
+      end
+
+      def debugger
+        `#{@content}.debugger`
+      end
     end
   end
 end
